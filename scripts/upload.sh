81dbfd2090552416ee5044863d54fcfa5f0bd3d0
Marius M <mariusmeschter99@gmail.com>
2020-01-23 22:42:13 +0100

    lol

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -4,4 +4,3 @@ maim -s /tmp/screenshot.png
 result=$(curl -s -X POST -H "Authorization: " -F "file=@/tmp/screenshot.png" https://)
 notify-send "Uploaded file to $result"
 echo "$result" | xclip -selection clipboard
-#wre0T#J3fjs
